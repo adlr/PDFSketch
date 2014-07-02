4a52335a29a94f90a601356d1e45e36241fa63e0
Andrew de los Reyes <adlr@chromium.org>
2014-07-01 18:05:33 -0700

    open new ui by default

--- main.js
+++ main.js
@@ -8,17 +8,14 @@ chrome.app.runtime.onLaunched.addListener(function() {
   // Center window on screen.
   var screenWidth = screen.availWidth;
   var screenHeight = screen.availHeight;
-  var width = 500;
+  var width = 700;
   var height = 500;
 
-  chrome.app.window.create('index.html', {
-    id: "PDFSketchID",
-    bounds: {
-      width: width,
-      height: height,
-      left: Math.round((screenWidth-width)/2),
-      top: Math.round((screenHeight-height)/2)
-    },
-    resizable: true
+  chrome.app.window.create('open.html', {
+    'id': "PDFSketchID",
+    'width': width,
+    'height': height,
+    'minWidth': width,
+    'minHeight': height,
   });
 });
